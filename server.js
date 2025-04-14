0a65b98913c3992d8f37604735d5029c22579777
azizulhoq953 <azizulhoq2041@gmail.com>
2025-04-14 14:59:19 +0600

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["http://localhost:5000","https://portfolioserver953.netlify.app/"], 
+  origin: ["http://localhost:5000","https://portfolio953.netlify.app/"], 
   credentials: true,
 }))
 const transporter = nodemailer.createTransport({
